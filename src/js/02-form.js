4c12e6f1946b2e7f312efb591cfd9ce34de7c1bb
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-03-03 14:10:56 +0300

    add simplelightbox dependency

---
+++ src/js/02-form.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', e => {
+    console.log(e.target.name);
+    console.log(form.elements);
+    const mail = form.elements.email.value;
+    const message = form.elements.message.value;
+    // console.log(mail);
+    // console.log(message);
+    localStorage.setItem('feedback-form-state', JSON.stringify({ mail, message }));
+});
+
+let data = localStorage.getItem('feedback-form-state');
+data = JSON.parse(data);
+if (data !== null) {
+    form.elements.email.value = data.mail;
+    form.elements.message.value = data.message;
+};
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(e);
+    localStorage.removeItem('feedback-form-state');
+    e.target.reset();
+});
